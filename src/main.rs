c706c747c3b9897011708a8a53ec590d2fb4203f
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2020-12-29 14:18:10 +0530

    Hook raytracer with pixels to make an interactive demo!
    WASD can be used to move the camera. The look direction can't be changed
    though.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::f32::consts::PI;
+
 use pixels::{Pixels, SurfaceTexture};
+use raycaster::{camera::Camera, map::Map, primitive::Vec2, raycaster::Raycaster};
 use winit::{
     dpi::LogicalSize,
     event::{Event, VirtualKeyCode},
@@ -9,6 +12,35 @@ use winit_input_helper::WinitInputHelper;
 
 const SCR_WD: u32 = 640;
 const SCR_HT: u32 = 480;
+const MAP_WD: usize = 24;
+const MAP_HT: usize = 24;
+const GRID: [u8; MAP_WD * MAP_HT] = [
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 7, 7, 7, 7, 7, 7, 7, 7, //
+    4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 7, //
+    4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, //
+    4, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, //
+    4, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 7, //
+    4, 0, 4, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7, 7, 0, 7, 7, 7, 7, 7, //
+    4, 0, 5, 0, 0, 0, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 7, 0, 0, 0, 7, 7, 7, 1, //
+    4, 0, 6, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 8, //
+    4, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 1, //
+    4, 0, 8, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 8, //
+    4, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 7, 7, 7, 1, //
+    4, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 0, 5, 5, 5, 5, 7, 7, 7, 7, 7, 7, 7, 1, //
+    6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, //
+    8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, //
+    6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, //
+    4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 6, 0, 6, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, //
+    4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, //
+    4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 2, 0, 0, 5, 0, 0, 2, 0, 0, 0, 2, //
+    4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, //
+    4, 0, 6, 0, 6, 0, 0, 0, 0, 4, 6, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 2, //
+    4, 0, 0, 5, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, //
+    4, 0, 6, 0, 6, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 5, 0, 0, 2, 0, 0, 0, 2, //
+    4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, //
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, //
+];
+const SPEED: f32 = 0.08;
 
 fn main() {
     let main_loop = EventLoop::new();
@@ -23,9 +55,20 @@ fn main() {
     let surf = SurfaceTexture::new(SCR_WD, SCR_HT, &window);
     let mut pixels = Pixels::new(SCR_WD, SCR_HT, surf).unwrap();
 
+    let mut camera = Camera::new(Vec2::new(5.0, 3.0), Vec2::new(1.0, 0.0), 90.0);
+    let map = Map {
+        wd: MAP_WD,
+        ht: MAP_HT,
+        grid: GRID.into(),
+    };
+    let caster = Raycaster {
+        scr_wd: SCR_WD,
+        scr_ht: SCR_HT,
+    };
+
     main_loop.run(move |event, _, cflow| {
         if let Event::RedrawRequested(_) = event {
-            draw_red_stripe(pixels.get_frame());
+            caster.render(&camera, &map, pixels.get_frame());
             pixels.render().unwrap();
         }
 
@@ -34,18 +77,31 @@ fn main() {
                 *cflow = ControlFlow::Exit;
                 return;
             }
+
+            let mut moved = false;
+
+            if input.key_held(VirtualKeyCode::W) {
+                camera.pos = camera.pos + camera.dir * SPEED;
+                moved = true;
+            }
+
+            if input.key_held(VirtualKeyCode::S) {
+                camera.pos = camera.pos - camera.dir * SPEED;
+                moved = true;
+            }
+
+            if input.key_held(VirtualKeyCode::A) {
+                camera.pos = camera.pos - camera.dir.rotated(PI / 2.0) * SPEED;
+                moved = true;
+            }
+            if input.key_held(VirtualKeyCode::D) {
+                camera.pos = camera.pos + camera.dir.rotated(PI / 2.0) * SPEED;
+                moved = true;
+            }
+
+            if moved {
+                window.request_redraw();
+            }
         }
     });
-}
-
-fn draw_red_stripe(buf: &mut [u8]) {
-    for x in 200..240 {
-        for y in 0..SCR_HT {
-            let i = 4 * (SCR_WD * y + x) as usize;
-            buf[i + 0] = 255;
-            buf[i + 1] = 0;
-            buf[i + 2] = 0;
-            buf[i + 3] = 255;
-        }
-    }
 }
